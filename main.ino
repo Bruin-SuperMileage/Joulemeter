622f3b2d2912e2c73fdfb64bb416c5b9217c569a
Brian Hardell <bharde@me.com>
2021-10-30 14:05:16 -0700

    Setup the main function file: 	- untested

---
+++ main.ino
@@ -0,0 +1,28 @@
+
+void setup() {
+  
+}
+
+void loop() {
+	// read voltage
+	// read current
+	// dynamically set gains
+	// sum proudct with time
+	// delay by a known time
+}
+
+
+
+void updateGains() {
+	// check if we're out of bounds of current gains
+	// update the gain profile by writing to the pins
+	// restart the device by throwing LOW then HIGH after a time delay specified in teh data sheet
+}
+
+double readCurrent() {
+	// communicating via SPI with the ADC
+}
+
+int readVoltage() {
+	
+}
